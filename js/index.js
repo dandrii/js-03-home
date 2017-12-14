ad70727061c616bcd0fe64610f0736fcc420d509
Andrii Doronin <doronin.andrii.lv@gmail.com>
2017-12-14 21:39:55 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -8,22 +8,22 @@ document.querySelectorAll('.operators button').forEach(el => {
 
 document.getElementById('result').addEventListener('click', result);
 
+document.getElementById('cancel').addEventListener('click', remove);
+
 function inputNumbers(ev) {
-    return document.getElementById("message").value += ev.target.dataset.tab;
+    document.getElementById("message").value += ev.target.dataset.tab;
 }
 
 function inputOperators(ev) {
-    if ((document.getElementById("message").value.length - 1) == '/')
-    {
-       // return document.getElementById("message").value.length - 1 = ev.target.dataset.tab;
-       alert (11111);
-    }
-    else {
-        return document.getElementById("message").value += ev.target.dataset.tab;
-    }
+    // let w = (document.getElementById("message").value.slice(0, 1))
+    document.getElementById("message").value += ev.target.dataset.tab;
 }
 
 function result(ev) {
     let res = eval(document.getElementById("message").value);
     document.getElementById("message").value = res;
+}
+
+function remove(ev) {
+    document.getElementById("message").value = ev.target.dataset.tab;
 }
